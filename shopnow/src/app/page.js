fb9e6eee4746c3a595e4f8595300dc70387e4c4e
Librapraise <alabipraise26@gmail.com>
2024-03-06 01:32:57 +0100

    updated the project

---
+++ shopnow/src/app/page.js
@@ -0,0 +1,15 @@
+import React from "react";
+import Hero from "./components/Hero";
+import NewProducts from "./components/NewProducts";
+import Testimonial from "./components/Testimonial";
+
+
+export default function Home() {
+  return (
+    <main>
+      <Hero />
+      <NewProducts />
+      <Testimonial />
+    </main>
+  );
+}
